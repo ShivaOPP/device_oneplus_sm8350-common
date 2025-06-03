cc42125508d91d14bea4a65790297a497e77ec55
mvimal2607 <mvimal2607@gmail.com>
2025-06-03 07:25:51 +0000

    sm8350-common: Don't build wvmkiller
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- common.mk
+++ common.mk
@@ -136,8 +136,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
-    wvmkiller
+    android.hardware.drm-service.clearkey
 
 # Fastboot
 PRODUCT_PACKAGES += \
